34adce20f0003cd895e41d94e49bce6104e44977
noorulain88 <noorulainnoorulain86@gmail.com>
2025-10-20 20:07:44 +0500

    assignment9

---
+++ progrma.js
@@ -0,0 +1,44 @@
+console.log("Hello world!")
+// line 01
+var username ="noor"
+console.log("username is ",username)
+var age=15
+console.log("user age  is ", age)
+// line 02
+var rollnum= 2417
+console.log(" rollnum of username", rollnum)
+var grade=10
+console.log("grade of user  is " , grade)
+// line 03
+// string
+var course="web development"
+console.log("user course is ", course)
+// number
+var rollnum= 2417
+console.log(" rollnum of username", rollnum)
+var grade=10
+console.log("grade of user  is " , grade)
+// boolean
+var housekeeper=false
+console.log("  user  is  a housekeeper" , housekeeper)
+var developer=true
+console.log("  user  is  a developer" , developer)
+// line 04,
+// valid and inavalid
+/*var null =valid
+var boolean=valid,in ,with
+var break=valid ,true and false
+var age=invalid
+var name =invalid,course,class,*/
+
+// line 05
+var a=3
+var b=3
+a
+
+    
+
+
+
+
+
