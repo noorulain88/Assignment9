3e1ec5697875ffd710f525da688cb007ecc541a0
noorulain88 <noorulainnoorulain86@gmail.com>
2025-10-21 19:06:02 +0500

    assignment 9 program

--- progrma.js
+++ progrma.js
@@ -1,15 +1,15 @@
 console.log("Hello world!")
-// line 01
+// program 01
 var username ="noor"
 console.log("username is ",username)
 var age=15
 console.log("user age  is ", age)
-// line 02
+// program 02
 var rollnum= 2417
 console.log(" rollnum of username", rollnum)
 var grade=10
 console.log("grade of user  is " , grade)
-// line 03
+// program 03
 // string
 var course="web development"
 console.log("user course is ", course)
@@ -23,7 +23,7 @@ var housekeeper=false
 console.log("  user  is  a housekeeper" , housekeeper)
 var developer=true
 console.log("  user  is  a developer" , developer)
-// line 04,
+// program 04,
 // valid and inavalid
 /*var null =valid
 var boolean=valid,in ,with
@@ -31,10 +31,66 @@ var break=valid ,true and false
 var age=invalid
 var name =invalid,course,class,*/
 
-// line 05
+// program 05
 var a=3
 var b=3
-a
+var sum=a+b
+console.log("sum of a and b is " ,sum)
+// program 06
+var a=3
+var b=3
+var sub=a-b
+console.log("subtraction of a and b is " ,sub)
+
+
+// program 07
+var a=3
+var b=9
+var multiply=a*b
+console.log("multiplication of a and b is " , multiply)
+
+
+// program 08
+var a=2
+var b=2
+var divide=a/b
+console.log("division of a and b is " , divide)
+var reminder=0
+console.log("so reminder is " ,reminder)  
+var quiotent=1
+console.log("so reminder is " ,quiotent)
+
+
+// program 09
+var length = 24
+var weidth=54
+var area=length*weidth
+console.log ("area of rectangle is" ,area )
+
+// program 10
+var length=4
+var square= length*length
+console.log("length of square is" , square)
+
+
+// program 11
+
+var a =9
+var b=6
+var c=7
+var average=a%b%c
+console.log("average of three number is " ,average)
+
+// program 13
+var a=100
+var b=9
+var comapre="a"
+console.log("")
+
+
+
+
+
 
     
 
