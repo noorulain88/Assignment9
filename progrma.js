09a4c0cf10906cc99019aa737595b866a85fd5f1
noorulain88 <noorulainnoorulain86@gmail.com>
2025-10-21 19:57:32 +0500

    program 17 16 ,

--- progrma.js
+++ progrma.js
@@ -23,7 +23,8 @@ var housekeeper=false
 console.log("  user  is  a housekeeper" , housekeeper)
 var developer=true
 console.log("  user  is  a developer" , developer)
-// program 04,
+// program 04
+
 // valid and inavalid
 /*var null =valid
 var boolean=valid,in ,with
@@ -36,6 +37,7 @@ var a=3
 var b=3
 var sum=a+b
 console.log("sum of a and b is " ,sum)
+
 // program 06
 var a=3
 var b=3
@@ -56,16 +58,16 @@ var b=2
 var divide=a/b
 console.log("division of a and b is " , divide)
 var reminder=0
-console.log("so reminder is " ,reminder)  
+console.log("So reminder is " ,reminder)  
 var quiotent=1
-console.log("so reminder is " ,quiotent)
+console.log("So reminder is " ,quiotent)
 
 
 // program 09
 var length = 24
 var weidth=54
 var area=length*weidth
-console.log ("area of rectangle is" ,area )
+console.log ("Area of rectangle is" ,area )
 
 // program 10
 var length=4
@@ -79,13 +81,86 @@ var a =9
 var b=6
 var c=7
 var average=a%b%c
-console.log("average of three number is " ,average)
+console.log("Average of three number is " ,average)
+// program 15
+var math=50
+var urdu =50
+var bio=50
+var eng=50
+var islamiat=50
+var total= 250
+console.log("Total number of math + urdu + eng + bio + islamiat" ,total)
 
-// program 13
+var math=50
+var urdu =50
+var bio=50
+var eng=50
+var islamiat=50
+var average= math % urdu % eng %bio % islamiat
+console.log("Average of 5 subject is ",average)
+// program 
+
+
+
+
+
+
+
+
+// program 16
+
+// arithematic operater
+// addition
+var y=5
+var r=8
+var sum=y+r
+console.log("sum of y and r is" ,sum)
+
+// subtraction
+var y=5
+var r=8
+var sub=y-r
+console.log("subtraction of y and r is" ,sub)
+
+// multiplication
+var t=6
+var u=7
+var multiply= t*u
+console.log("multiplicaton of t and u is equal to " ,multiply)
+
+// division
+var t=6
+var u=6
+var division=t/u
+console.log("division  of t and u is equal to " ,division )
+
+//  modulus
+var e=7
+var h=8
+var modulus=e%h
+console.log("modulus of e and h is " , modulus)
+
+
+// program 17
+var age1=56
+var age2=54
+var comparision=age1>age2
+console.log("check age1 is greather then age2" , comparision)
+var age1=89
+var age2=98
+var comparision=age1<age2
+console.log("check age1 is greather then age2" , comparision)
+
+
+
+// program 18
 var a=100
-var b=9
-var comapre="a"
-console.log("")
+var b=100
+
+console.log("check a and b are equal" , a==b)
+var u=7
+var v=9
+console.log("check u and v are equal" , u==v)
 
 
 
