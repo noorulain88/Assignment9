ddc7396e5a9562a50543d4b6252455dc087d6376
noorulain88 <noorulainnoorulain86@gmail.com>
2025-10-25 19:10:19 +0500

    assignmnet 9

--- progrma.js
+++ progrma.js
@@ -1,33 +1,47 @@
 console.log("Hello world!")
 // program 01
 
-var username ="Noor"
-console.log("   Username is ",username)
-var age=15
+var username = "Noor"
+console.log("   Username is ", username)
+var age = 15
 console.log("   User age  is ", age)
 
 
 // program 02
-var rollnum= 2417
+var rollnum = 2417
 console.log("Rollnum of user is ", rollnum)
-var grade=10
-console.log("Grade of User  is " , grade)
+var grade = 10
+console.log("Grade of User  is ", grade)
 
 
 // program 03
 // string
-var course="Web Development"
+var course = "Web Development"
 console.log("User Course is ", course)
 // number
-var rollnum= 2417
+var rollnum = 2417
 console.log(" RollNum of User is", rollnum)
-var grade=10
-console.log("   Gade of User  is " , grade)
+var grade = 10
+console.log("   Gade of User  is ", grade)
 // boolean
-var housekeeper=false
-console.log("  User  is  a Housekeeper" , housekeeper)
-var developer=true
-console.log("  User  is  a Developer" , developer)
+
+var housekeeper = prompt("Is user a housekeeper? true or false")
+if(housekeeper=="true "){
+    console.log("user is a housekeeper")
+}
+else{
+    console.log("user is not a housekeeper")
+}
+var developer=prompt("Is user a Developer? true or false")
+
+
+
+if(developer=="true"){
+    console.log("user is a Developer")
+}
+else{
+    console.log("user is not a Developer")
+}
 
 
 // program 04
@@ -40,108 +54,120 @@ var age=invalid
 var name =invalid,course,class,*/
 
 // program 05
-var a=3
-var b=3
-var sum=a+b
-console.log("   Sum of a and b is " ,sum)
+var a = 3
+var b = 3
+var sum = a + b
+console.log("   Sum of a and b is ", sum)
 
 
 // program 06
-var a=3
-var b=3
-var sub=a-b
-console.log("Subtraction of a and b is " ,sub)
+var a = 3
+var b = 3
+var sub = a - b
+console.log("Subtraction of a and b is ", sub)
 
 
 // program 07
-var a=3
-var b=9
-var multiply=a*b
-console.log("    Multiplication of a and b is " , multiply)
+var a = 3
+var b = 9
+var multiply = a * b
+console.log("    Multiplication of a and b is ", multiply)
 
 
 // program 08
-var a=2
-var b=2
-var divide=a/b
-console.log("Division of a and b is " , divide)
-var reminder=0
-console.log("  So reminder is " ,reminder)  
-var quiotent=1
-console.log("So reminder is " ,quiotent)
+var a = 2
+var b = 2
+var divide = a / b
+console.log("Division of a and b is ", divide)
+var reminder = 0
+console.log("  So reminder is ", reminder)
+var quiotent = 1
+console.log("So reminder is ", quiotent)
 
 
 // program 09
 var length = 24
-var weidth=54
-var area=length*weidth
-console.log ("Area of Rctangle is" ,area )
+var weidth = 54
+var area = length * weidth
+console.log("Area of Rctangle is", area)
 
 // program 10
-var length=4
-var square= length*length
-console.log("Length of Square is" , square)
+var length = 4
+var square = length * length
+console.log("Length of Square is", square)
 
 
 // program 11
 
-var a =9
-var b=6
-var c=7
-var average=a%b%c
-console.log("Average of three Number is " ,average)
- 
+var a = 9
+var b = 6
+var c = 7
+var average = a % b % c
+console.log("Average of three Number is ", average)
+
 // program 12
-var chair1=1
-var chair2=2
-var swap1=++chair2
+var chair1 = 1
+var chair2 = 2
+var swap1 = ++chair2
 console.log("swap value of chair 2 into chair 3 ", swap1)
 
-var chair1=1
-var swap2=++chair1
+var chair1 = 1
+var swap2 = ++chair1
 console.log("swap value of chair 1 into chair 2", swap2)
 
-var chair3=3
-var swap3=--chair1
+var chair3 = 3
+var swap3 = --chair1
 console.log("swap value of chair3  into chair1 ", swap3)
 
-   
+
 
 //   program 13
-var num1=100
-var num2=5
-console.log(" A number is Greater then 100 or not",num1<num2 )
+var num1 = 100
+var num2 = 5
+if (num1 < num2){
+    console.log("number is greater then 100")
+}
+else(
+    console.log("number is not greater then 100")
+)
 
-var num1=100
-var num2=5
-console.log(" A number is Greater then 100 or not",num2<num1 )
+
+var num1 = 100
+var num2 = 5
+if (num1 > num2){
+    console.log("number is greater then 100")
+}
+else(
+    console.log("number is not greater then 100")
+)
 
 // program 14
-var t=4
-var u=6
-var compare=t<u
-console.log("comparision between two numbers" ,compare)
-
+var t = 4
+var u = 6
+var compare = t < u
+if(compare==true){
+    console.log("t is less then u", )
+}
 
 
 
 
 // program 15
-var math=50
-var urdu =50
-var bio=50
-var eng=50
-var islamiat=50
-var total= 250
-console.log("Total Number of math + urdu + eng + bio + islamiat" ,total)
+var math = 50
+var urdu = 50
+var bio = 50
+var eng = 50
+var islamiat = 50
+var total = 250
+console.log("Total Number of math + urdu + eng + bio + islamiat", total)
 
-var math=50
-var urdu =50
-var bio=50
-var eng=50
-var islamiat=50
-var average= math % urdu % eng % bio % islamiat
-console.log("Average of 5 subject is ",average)
+var math = 50
+var urdu = 50
+var bio = 50
+var eng = 50
+var islamiat = 50
+var average = math % urdu % eng % bio % islamiat
+console.log("Average of 5 subject is ", average)
 
 
 
@@ -149,59 +175,82 @@ console.log("Average of 5 subject is ",average)
 
 // arithematic operater
 // addition
-var y=5
-var r=8
-var sum=y+r
-console.log("sum of y and r is" ,sum)
+var y = 5
+var r = 8
+var sum = y + r
+console.log("sum of y and r is", sum)
 
 // subtraction
-var y=5
-var r=8
-var sub=y-r
-console.log("subtraction of y and r is" ,sub)
+var y = 5
+var r = 8
+var sub = y - r
+console.log("subtraction of y and r is", sub)
 
 // multiplication
-var t=6
-var u=7
-var multiply= t*u
-console.log("multiplicaton of t and u is equal to " ,multiply)
+var t = 6
+var u = 7
+var multiply = t * u
+console.log("multiplicaton of t and u is equal to ", multiply)
 
 // division
-var t=6
-var u=6
-var division=t/u
-console.log("division  of t and u is equal to " ,division )
+var t = 6
+var u = 6
+var division = t / u
+console.log("division  of t and u is equal to ", division)
 
 //  modulus
-var e=7
-var h=8
-var modulus=e%h
-console.log("modulus of e and h is " , modulus) 
+var e = 7
+var h = 8
+var modulus = e % h
+console.log("modulus of e and h is ", modulus)
 
 //  exponent
-var h=8
-var exponent=h**3
-console.log("exponent of e and h is " ,exponent ) 
+var h = 8
+var exponent = h ** 3
+console.log("exponent of e and h is ", exponent)
 
 
 // program 17
-var age1=52
-var age2=54
-var comparision=age1>age2
-console.log("check age1 is greather then age2" , comparision)
-var age1=89
-var age2=98
-var comparision=age2>age1
-console.log("check age1 is greather then age2" , comparision)
+var age1 = 52
+var age2 = 54
+var comparision = age1 > age2
+if(comparision==true){
+    console.log("age1 is greather then age2",)
+}
+else{
+    console.log("age1 is not greather then age2",)
+}
+
+
+var age1 = 89
+var age2 = 98
+var comparision = age2 > age1
+if(comparision==true){
+    console.log("age2 is greather then age1",)
+}
+else{
+    console.log("age2 is not greather then age1",)
+}
 
 
 
 // program 18
-var a=100
-var b=100
+var a = 100
+var b = 100
+if(a ==b){
+    console.log("check a and b are equal", )
+}
+else{
+    console.log("check a and b are not equal", )
+}
+var u = 7
+var v = 9
+if(u ==v){
+    console.log("check u and v are equal", )
+}
+else{
+    console.log("check a and b are not equal", )
+}
 
-console.log("check a and b are equal" , a==b)
-var u=7
-var v=9
-console.log("check u and v are equal" , u==v)
+
 
